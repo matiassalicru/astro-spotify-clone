fcff2d6b34e4d75bf7cf5f4208eb5f0517a18889
Matias Salicru <40377128+matiassalicru@users.noreply.github.com>
2024-04-21 13:36:58 -0300

    feature: add sidebar, main page, music, fonts, images, icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()],
+  output: 'server',
 });
