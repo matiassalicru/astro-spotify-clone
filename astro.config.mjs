bc1b9571b50d239958e5dd06fbcdea54f0087280
Matias Salicru <40377128+matiassalicru@users.noreply.github.com>
2024-04-21 15:31:34 -0300

    feature: add audio with React and a Greeting with Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: 'server',
+  integrations: [tailwind(), icon(), svelte(), react()],
+  output: 'server'
 });
---
+++ src/components/Player.jsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect } from 'react';
+
+const Play = () => (
+  <svg role='img' viewBox='0 0 16 16' width={16} height={16}>
+    <path d='M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z'></path>
+  </svg>
+);
+
+const Pause = () => (
+  <svg role='img' viewBox='0 0 16 16' width={16} height={16}>
+    <path d='M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
+  </svg>
+);
+
+export function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current.src = '/src/music/1/01.mp3';
+  }, []);
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying((play) => !play);
+  };
+
+  return (
+    <div className='flex flex-row justify-between w-full px-4 z-50'>
+      <div>currentSong..</div>
+
+      <div className='grid place-content-center gap-4 flex-1'>
+        <div className='flex justify-center'>
+          <button
+            className='bg-white rounded-full p-2'
+            onClick={() => handleClick()}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
+          <audio ref={audioRef} />
+        </div>
+      </div>
+
+      <div className='grid place-content-center'></div>
+    </div>
+  );
+}
